e334249ca9e385a4b0ed536c395d4812bfbfee00
Carolyn Berard <carolynmberard@gmail.com>
2021-03-31 15:21:21 -0500

    Added some image assets for page backgrounds

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import background from "../images/landing-bg.png"
+
+const HomePage = () => {
+  return (
+    <main>
+      <img alt="" src={background} />
+      <h1>My Home Page</h1>
+    </main>
+  )
+}
+
+export default HomePage
